d884e7edc07b2e527cf255efc44d3654c66d0853
antucarr <antucar@icloud.com>
2023-10-06 04:15:23 +0300

    updated frontend.

---
+++ app/src/main/java/com/ImageX/ImageX/view/HomeScreenFragment.kt
@@ -0,0 +1,36 @@
+package com.ImageX.ImageX.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.ImageX.ImageX.R
+
+
+class HomeScreenFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val searchbutton = view.findViewById<Button>(R.id.searchbutton)
+        searchbutton.setOnClickListener {
+            findNavController().navigate(HomeScreenFragmentDirections.actionHomeScreenToArtFragment())
+        }
+    }
+}
